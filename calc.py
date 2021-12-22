9e2cc0d3caca2878a0a0a99704fd5b2cd65ee902
Yohan <muliono.yohan@gmail.com>
2021-12-22 16:29:20 +0700

    menambahkan fungsi pangkatdua

--- calc.py
+++ calc.py
@@ -12,10 +12,14 @@ def perkalian(a,b):
 def pembagian(a,b):
 	total = a/b
 	return total
+def pangkatdua(a):
+	total = a*a
+	return total
 
 def main():
 	print (penambahan(10,5))
 	print (pengurangan(10,5))
 	print (perkalian(10,5))
 	print (pembagian(10,5))
+	print (pangkatdua(5))
 main()
