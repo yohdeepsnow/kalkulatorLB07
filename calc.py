e00f3dd3f19390be2bdd44106a5ad5afd8597816
cocatrip <cocainetrip@protonmail.com>
2021-12-22 16:35:00 +0700

    test

--- calc.py
+++ calc.py
@@ -1,26 +1,2 @@
-def penambahan(a,b):
-	total = a+b
-	return total
-
-def pengurangan(a,b):
-	total = a-b
-	return total
-
-def perkalian(a,b):
-	total = a*b
-	return total
-def pembagian(a,b):
-	total = a/b
-	return total
-
-def pangkatdua(b):
-	total = b*b
-	return total
-
-def main():
-	print (penambahan(10,5))
-	print (pengurangan(10,5))
-	print (perkalian(10,5))
-	print (pembagian(10,5))
-	print (pangkatdua(5))
-main()
+def pangkatpangkat(a):
+    return a * a
